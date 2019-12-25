e86310a021ccd461ba7cf55ef425f8f4009ce7b6
Parth Agrawal <parth.agrawal@outlook.com>
2019-12-25 12:36:24 -0500

    Now adds rectangles to an array

--- fractals.py
+++ fractals.py
@@ -7,24 +7,26 @@ x_values = [1, 2, 3, 7, 4]
 y_values = [5, 2, 4, 5, 1]
 plt.plot(x_values, y_values)
 
-# xy = (2,2)
-# r1 = pat.Rectangle(xy, 2, 2)
-# ax.add_patch(r1)
 
-q_x_min = 4 * min(x_values)
-q_x_max = 4 * max(x_values)
+# This is because the range() function can't handle floats.
+# Set box size here and increase the min/max by the corresponding 1/b_size ratio.
+b_size = .25
+quant = int(1/b_size)
 
-q_y_min = 4 * min(y_values)
-q_y_max = 4 * max(y_values)
+q_x_min = quant * min(x_values)
+q_x_max = quant * max(x_values)
 
+q_y_min = quant * min(y_values)
+q_y_max = quant * max(y_values)
 
-# x_range = x_max - x_min
-# y_range = y_max - y_min
+r_arr = []
 
+# Adds rectangles to an array and adds to plot 
 for i in range(q_x_min, q_x_max, 1) :
     for j in range(q_y_min, q_y_max, 1) :
-        xy = (i/4, j/4)
-        rect = pat.Rectangle(xy, .25, .25, fill = False)
+        xy = (i/quant, j/quant)
+        rect = pat.Rectangle(xy, b_size, b_size, fill = False)
+        r_arr.append(rect)
         ax.add_patch(rect)
 
 
